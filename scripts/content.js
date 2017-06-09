e56d4399d1b0cb2be150614b1c6177d529e29fc5
Nick Pagsanjan <nickchunkster@gmail.com>
2017-06-09 13:54:15 -0700

    Experimental features

--- scripts/content.js
+++ scripts/content.js
@@ -1,15 +1,28 @@
-chrome.runtime.onMessage.addListener(updateString);
-
-function updateString(message) {
+var currentHighlight = 0;
+var matches = 0;
+chrome.runtime.onMessage.addListener(function (message) {
     removeHighlights(document.body);
-    if (message.regex != '') {
+    if (message.type === 'highlight' && message.regex != '') {
         highlightWord(document.body, message.regex);
+    } else if (message.type === 'remove_highlights') {
+        removeHighlights(document.body);
+    } else if (message.type === 'cycle') {
+        if (currentHighlight > matches) {
+            currentHighlight = 0;
+        }
+        cycleHighlight(document.body);
     }
-}
+});
 
 function highlightWord(root, word) {
     var re = new RegExp(word, 'g');
+    var numMatches = 0;
+    matches = numMatches;
     textNodesUnder(root).forEach(highlightWords);
+    chrome.runtime.sendMessage({
+        from:    'content',
+        numMatches: numMatches
+    });
 
     function textNodesUnder(root) {
         var walk = document.createTreeWalker(root, NodeFilter.SHOW_TEXT, null, false),
@@ -26,16 +39,30 @@ function highlightWord(root, word) {
                 var after = n.splitText(i + match[0].length);
                 var highlighted = n.splitText(i);
                 var span = document.createElement('span');
-                span.className = 'highlighted';
-                span.appendChild(highlighted);
+
+                if (numMatches === currentHighlight) {
+                    span.className = 'current';
+                    span.appendChild(highlighted);
+                } else {
+                    span.className = 'highlighted';
+                    span.appendChild(highlighted);
+                }
                 after.parentNode.insertBefore(span, after);
+                numMatches += 1;
             }
         }
     }
 }
 
 function removeHighlights(root) {
-    [].forEach.call(root.querySelectorAll('span.highlighted'), function(el) {
+    [].forEach.call(root.querySelectorAll('span.highlighted, span.current'), function(el) {
         el.parentNode.replaceChild(el.firstChild, el);
     });
+}
+
+function cycleHighlight(root) {
+    var span = root.querySelector('span.current')
+    if (span != null) {
+        span.className = 'highlighted';
+    }
 }
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,6 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("send").addEventListener("click", popup);
+    document.getElementById("remove").addEventListener("click", remove);
+    document.getElementById("cycle").addEventListener("click", cycle);
 
-chrome.commands.onCommand.addListener(function(command) {
-  console.log('onCommand event received for message: ', command);
+    chrome.commands.onCommand.addListener(function(command) {
+        console.log('onCommand event received for message: ', command);
+    });
+
+    chrome.runtime.onMessage.addListener(function (message) {
+        if (msg.from === 'content' && msg.numMatches != 0) {
+            document.getElementById('matches').innerHTML = 'Matches ' + message.numMatches;
+        }
+    });
 });
 
 function popup() {
@@ -13,32 +24,51 @@ function popup() {
             file: "scripts/content.js"
         }, function() {
             chrome.tabs.sendMessage(tabs[0].id, {
+                type: "highlight",
                 regex: regex
             });
         });
     });
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("send").addEventListener("click", popup);
+function remove() {
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }, function(tabs) {
+        chrome.tabs.executeScript(tabs[0].id, {
+            file: "scripts/content.js"
+        }, function() {
+            chrome.tabs.sendMessage(tabs[0].id, {
+                type: "remove_highlights"
+            });
+        });
+    });
+}
 
-});
+function cycle() {
+    var regex = document.getElementById("regex").value;
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }, function(tabs) {
+        chrome.tabs.executeScript(tabs[0].id, {
+            file: "scripts/content.js"
+        }, function() {
+            chrome.tabs.sendMessage(tabs[0].id, {
+                type: "cycle",
+                regex: regex
+            });
+        });
+    });
+}
 
 $('#text').on('input', function() {
     document.getElementById("send").click();
 });
 
 $(document).keypress(function(e) {
-    if(e.which == 13) { // ENTER key
+    if (e.which == 13) { // ENTER key
         document.getElementById("send").click();
     }
-});
-
-$('send').on('click', function() {
-    $(this).prop('disabled', true);
-    setTheme('regexpal')
-});
-
-$("#idContentEditable").keypress(function(e) {
-    return e.which != 13;
 });
