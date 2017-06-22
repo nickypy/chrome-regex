b939dbedf26af5ae458fdbe5f6d05557a366026d
Nick Pagsanjan <nickchunkster@gmail.com>
2017-06-22 09:53:09 -0700

    Migrating to mark.js

--- scripts/content.js
+++ scripts/content.js
@@ -12,42 +12,13 @@ chrome.runtime.onMessage.addListener(function (message) {
     }
 });
 
-// TODO   function freezes page for large pages and numerous matches
-// BUG    clearing highlights remoes words
-// MAYBE  migrate to mark.js
 function highlightWord(root, word) {
     var re = new RegExp(word, 'g');
     var numMatches = 0;
 
-    textNodesUnder(root).forEach(highlightWords);
-    return numMatches;
 
-    function textNodesUnder(root) {
-        var walk = document.createTreeWalker(root, NodeFilter.SHOW_TEXT, null, false),
-            text = [],
-            node;
-        while (node = walk.nextNode()) text.push(node);
-        return text;
-    }
-
-    function highlightWords(n) {
-        var match = re.exec(n.nodeValue);
-        if (match != null) {
-            for (var i; (i = n.nodeValue.indexOf(match[0], match.index)) > -1; n = after) {
-                var after = n.splitText(i + match[0].length);
-                var highlighted = n.splitText(i);
-                var span = document.createElement('span');
-                span.className = 'highlighted';
-                span.appendChild(highlighted);
-                after.parentNode.insertBefore(span, after);
-                numMatches += 1;
-            }
-        }
-    }
 }
 
 function removeHighlights(root) {
-    [].forEach.call(root.querySelectorAll('span.highlighted'), function(el) {
-        el.parentNode.replaceChild(el.firstChild, el);
-    });
+
 }
