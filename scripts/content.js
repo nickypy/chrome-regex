12174f87a4d1ff139097c67053b007c05d306d13
Nick Pagsanjan <nickchunkster@gmail.com>
2017-08-30 16:52:19 -0700

    Refeactored using JSPrettier

--- scripts/content.js
+++ scripts/content.js
@@ -1,18 +1,18 @@
 document.onload = function() {
-    var markjs = document.createElement('script');
-    markjs.src = 'mark.min.js';
+    var markjs = document.createElement("script");
+    markjs.src = "mark.min.js";
 
-    var jquery = document.createElement('script');
-    jquery.src = 'jquery-3.2.1.min.js';
+    var jquery = document.createElement("script");
+    jquery.src = "jquery-3.2.1.min.js";
 
-    var style = document.createElement('link');
+    var style = document.createElement("link");
     style.rel = "stylesheet";
-    style.href = '../styles/content.css';
+    style.href = "../styles/content.css";
 
     document.body.appendChild(markjs);
     document.body.appendChild(styles);
     document.body.appendChild(jquery);
-}
+};
 
 var context = document.querySelector("body"),
     instance = new Mark(context),
@@ -20,28 +20,32 @@ var context = document.querySelector("body"),
     currentIndex = 0,
     results;
 
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    if (message.type === 'highlight' && message.regex != '') {
-        let re = new RegExp(message.regex, 'g');
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    if (message.type === "highlight" && message.regex != "") {
+        let re = new RegExp(message.regex, "g");
         instance.unmark();
         instance.markRegExp(re);
-        results = document.querySelectorAll('mark');
+        results = document.querySelectorAll("mark");
         currentIndex = 0;
         jumpTo();
-    } else if (message.type === 'remove_highlights') {
+    } else if (message.type === "remove_highlights") {
         instance.unmark();
-    } else if (message.type === 'next') {
+    } else if (message.type === "next") {
         if (results.length) {
-            results[currentIndex].classList.remove('current');
+            results[currentIndex].classList.remove("current");
             currentIndex += 1;
-            currentIndex = currentIndex % (results.length - 1);
-            console.log(currentIndex);
+            currentIndex = currentIndex % results.length;
         }
         jumpTo();
     }
 });
 
-
-
-
-
+function jumpTo() {
+    if (results.length) {
+        var position,
+            current = results[currentIndex];
+        current.classList.add("current");
+        position = current.offsetTop;
+        window.scrollTo(0, position);
+    }
+}
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,62 +1,85 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.addEventListener("keypress", function(e) {
-        if (e.which == 13) { // ENTER key
+        if (e.which == 13) {
+            // ENTER key
             document.getElementById("send").click();
         }
     });
 
     document.getElementById("send").addEventListener("click", function() {
         var regexp = document.getElementById("regex").value;
-        chrome.tabs.query({
-            active: true,
-            currentWindow: true
-        }, function(tabs) {
-            chrome.tabs.executeScript(tabs[0].id, {
-                file: "scripts/content.js"
-            }, function() {
-                chrome.tabs.sendMessage(tabs[0].id, {
-                    type: "highlight",
-                    regex: regexp
-                });
-            });
-        });
+        chrome.tabs.query(
+            {
+                active: true,
+                currentWindow: true
+            },
+            function(tabs) {
+                chrome.tabs.executeScript(
+                    tabs[0].id,
+                    {
+                        file: "scripts/content.js"
+                    },
+                    function() {
+                        chrome.tabs.sendMessage(tabs[0].id, {
+                            type: "highlight",
+                            regex: regexp
+                        });
+                    }
+                );
+            }
+        );
     });
 
     document.getElementById("next").addEventListener("click", function() {
         var regexp = document.getElementById("regex").value;
-        chrome.tabs.query({
-            active: true,
-            currentWindow: true
-        }, function(tabs) {
-            chrome.tabs.executeScript(tabs[0].id, {
-                file: "scripts/content.js"
-            }, function() {
-                chrome.tabs.sendMessage(tabs[0].id, {
-                    type: "next",
-                    regex: regexp
-                });
-            });
-        });
+        chrome.tabs.query(
+            {
+                active: true,
+                currentWindow: true
+            },
+            function(tabs) {
+                chrome.tabs.executeScript(
+                    tabs[0].id,
+                    {
+                        file: "scripts/content.js"
+                    },
+                    function() {
+                        chrome.tabs.sendMessage(tabs[0].id, {
+                            type: "next",
+                            regex: regexp
+                        });
+                    }
+                );
+            }
+        );
     });
 
     document.getElementById("remove").addEventListener("click", function() {
-        chrome.tabs.query({
-            active: true,
-            currentWindow: true
-        }, function(tabs) {
-            chrome.tabs.executeScript(tabs[0].id, {
-                file: "scripts/content.js"
-            }, function() {
-                chrome.tabs.sendMessage(tabs[0].id, {
-                    type: "remove_highlights"
-                });
-            });
-        });
+        chrome.tabs.query(
+            {
+                active: true,
+                currentWindow: true
+            },
+            function(tabs) {
+                chrome.tabs.executeScript(
+                    tabs[0].id,
+                    {
+                        file: "scripts/content.js"
+                    },
+                    function() {
+                        chrome.tabs.sendMessage(tabs[0].id, {
+                            type: "remove_highlights"
+                        });
+                    }
+                );
+            }
+        );
     });
 
     chrome.runtime.onMessage.addListener(function(message) {
-        if (message.from === 'content' && message.matches != 0) {
-            document.getElementById('matches').innerHTML = 'Matches: ' + message.matches;
+        if (message.from === "content" && message.matches != 0) {
+            document.getElementById("matches").innerHTML =
+                "Matches: " + message.matches;
         }
     });
 });
